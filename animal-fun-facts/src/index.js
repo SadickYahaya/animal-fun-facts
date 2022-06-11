3b24bc808f277941389722eaa3c09166003bdb4f
Sadick Abubakar Yahaya <abubakar.yahaya@amalitech.org>
2022-06-11 00:52:01 +0000

    adding files into react app

---
+++ animal-fun-facts/src/index.js
@@ -0,0 +1,54 @@
+import { animals } from './animals';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+import Ocean from './images/ocean.jpg';
+// import Lobster from './images/lobster.jpg';
+// import Starfish from './images/starfish.jpg';
+// import Dolphin from'./images/dolphin.jpg';
+
+
+
+const title = "";
+
+const background = (
+  <img className="background" alt="ocean" src={Ocean} />
+);
+
+const images = [];
+
+for (let animal in animals){
+  images.push(
+    <img key = {animal} 
+    className = "animal" 
+    alt = {animal}
+    src = {animals[animal].image} 
+    ariaLabel = {animal} 
+    role = "button"
+    onClick = {displayFact}
+    />
+  )
+}
+
+const animalFacts = (
+  <div>
+    <h1>
+      {title === "" ? 'Click an animal for a fun fact' : title}
+    </h1>
+    {background}
+    <p id='fact'></p>
+    <div className="animals">
+      {images}
+    </div>
+  </div>
+);
+
+function displayFact (e) {
+ const selectedAnimal = e.target.alt;
+ const animalInfo = animals[selectedAnimal];
+ const randomFact = Math.floor(Math.random() * animalInfo.facts.length);
+ const funFact = animalInfo.facts(randomFact);
+document.getElementById('fact').innerHTML = funFact;
+}
+
+ReactDOM.render(animalFacts, document.getElementById('root'));
